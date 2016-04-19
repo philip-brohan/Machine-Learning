cf9863a6d8226be499086addd9a1c1de5e83148a
Philip Brohan <philip@brohan.org>
2016-04-19 23:29:00 +0100

    Fit the net and look at the reconstructed AT.

--- local_weather/fit_t2m.R
+++ local_weather/fit_t2m.R
@@ -7,8 +7,6 @@ library(lubridate)
 
 date.c<-ymd('2014-01-01')
 
-prate<-TWCR.get.slice.at.hour('prate',2014,1,1,0,version='3.5.1')
-
 # switch from 0-360 to -180,180 longitudes
 centre.on.greenwich<-function(field) {
     p2<-field
@@ -69,25 +67,55 @@ fill.var<-function(var,grid,n.days,type='mean') {
 }
 
 standardise<-function(mean,normal,sd) {
-  mean$data[]<-(mean$data-normal$data)/(sd$data*3)
+  mean$data[]<-(mean$data-normal$data)/(sd$data*6)+0.5
   mean$data[]<-pmax(0,mean$data)
   mean$data[]<-pmin(1,mean$data)
   return(mean)
 }
 
-# Use the precip grid, trimmed to the UK region
+# Use the air.2m grid, trimmed to the UK region
 grid<-trim(centre.on.greenwich(
-         TWCR.get.slice.at.hour('prate',2014,1,1,0,version='3.5.1')))
+         TWCR.get.slice.at.hour('air.2m',2014,1,1,0,version='3.5.1')))
   
 # Get the variables of interest - need normals and sds for standardisation
 
-prmsl<-fill.var('prmsl',grid,100,'mean')
-prmsl.n<-fill.var('prmsl',grid,100,'normal')
-prmsl.sd<-fill.var('prmsl',grid,100,'standard.deviation')
-prmsl.std<-standardise(prmsl,prmsl.n,prmls.sd)
+prmsl<-fill.var('prmsl',grid,365,'mean')
+prmsl.n<-fill.var('prmsl',grid,365,'normal')
+prmsl.sd<-fill.var('prmsl',grid,365,'standard.deviation')
+prmsl.std<-standardise(prmsl,prmsl.n,prmsl.sd)
 
+air.2m<-fill.var('air.2m',grid,365,'mean')
+air.2m.n<-fill.var('air.2m',grid,365,'normal')
+air.2m.sd<-fill.var('air.2m',grid,365,'standard.deviation')
+air.2m.std<-standardise(air.2m,air.2m.n,air.2m.sd)
 
+uwnd.10m<-fill.var('uwnd.10m',grid,365,'mean')
+uwnd.10m.n<-fill.var('uwnd.10m',grid,365,'normal')
+uwnd.10m.sd<-fill.var('uwnd.10m',grid,365,'standard.deviation')
+uwnd.10m.std<-standardise(uwnd.10m,uwnd.10m.n,uwnd.10m.sd)
 
-nn<-neuralnet(case~age+parity+induced+spontaneous,
-              data=infert,hidden=2,err.fct='ce',
-              linear.output=FALSE)
+vwnd.10m<-fill.var('vwnd.10m',grid,365,'mean')
+vwnd.10m.n<-fill.var('vwnd.10m',grid,365,'normal')
+vwnd.10m.sd<-fill.var('vwnd.10m',grid,365,'standard.deviation')
+vwnd.10m.std<-standardise(vwnd.10m,vwnd.10m.n,vwnd.10m.sd)
+
+# Pack the standardised data into a data frame and fit the model
+ukw<-data.frame(slp=as.vector(prmsl.std$data),
+                at=as.vector(air.2m.std$data),
+		u=as.vector(uwnd.10m.std$data),
+		v=as.vector(vwnd.10m.std$data))
+
+nn<-neuralnet(at~slp+u+v,data=ukw,hidden=2)
+
+plot(hexbin(as.vector(air.2m.std$data),as.vector(nn$net.result[[1]])))
+
+fitted<-air.2m
+fitted$data[]<-as.vector(nn$net.result[[1]])
+
+compare.fit<-function(n,...) {
+
+   f.in<-GSDF.reduce.1d(air.2m.std,'time',function(x){return(x[n])})
+   f.out<-GSDF.reduce.1d(fitted,'time',function(x){return(x[n])})
+   GSDF.pplot.2d(f.in,f.out,...)
+
+}
