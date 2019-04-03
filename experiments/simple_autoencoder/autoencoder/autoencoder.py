67a2a31ce465693c39481638ff49f5ba9c4a8e57
Philip Brohan <philip@brohan.org>
2019-04-03 14:04:18 +0100

    Started autoencoder script.

---
+++ experiments/simple_autoencoder/autoencoder/autoencoder.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Simple autoencoder with a single small, fully-connected layer
+# Unlikely to work well at all - this is just to learn how to
+#  get 20CR data in and out, setup, train and test a model.
+
+
+import tensorflow as tf
+import numpy as np
