4283987e047c305d243ce3cf9d5e7ddc4092cdd5
Philip Brohan <philip@brohan.org>
2019-05-09 13:26:31 +0100

    Make view_weights show the hidden layer weights.

--- experiments/simple_autoencoder_introspection/9_neurons/view_weights.py
+++ experiments/simple_autoencoder_introspection/9_neurons/view_weights.py
@@ -26,12 +26,13 @@ import cartopy.crs as ccrs
 # Where on the plot to put each axes
 def axes_geom(layer=0,channel=0,nchannels=9):
      if layer==0: 
-         base=[0.0,0.5,1.0,0.5]
+         base=[0.0,0.6,1.0,0.4]
      else:
-         base=[0.0,0.0,1.0,0.5]
+         base=[0.0,0.0,1.0,0.4]
      ncol=math.sqrt(nchannels)
      nr=channel//ncol
      nc=channel-ncol*nr
+     nr=ncol-1-nr # Top down
      geom=[base[0]+(base[2]/ncol)*0.95*nc,
            base[1]+(base[3]/ncol)*0.95*nr,
            (base[2]/ncol)*0.95,
@@ -41,7 +42,8 @@ def axes_geom(layer=0,channel=0,nchannels=9):
      return geom
 
 # Plot a single set of weights
-def plot_weights(weights,layer=0,channel=0,nchannels=9):
+def plot_weights(weights,layer=0,channel=0,nchannels=9,
+                 vmin=None,vmax=None):
     ax_input=fig.add_axes(axes_geom(layer=layer,
                                     channel=channel,
                                     nchannels=nchannels),
@@ -54,36 +56,50 @@ def plot_weights(weights,layer=0,channel=0,nchannels=9):
     lons = w_in.coord('longitude').points-180
     prate_img=ax_input.pcolorfast(lons, lats, w_in.data, 
                             cmap='coolwarm',
-                            vmin=numpy.mean(w_in.data)-numpy.std(w_in.data)*3,
-                            vmax=numpy.mean(w_in.data)+numpy.std(w_in.data)*3,
+                            vmin=vmin,
+                            vmax=vmax,
                             )
 
     # Give the mean and sd
-    mg.utils.plot_label(ax_input,
-                  '%5.2f %5.2f' % (numpy.mean(w_in.data),
-                                              numpy.std(w_in.data)),
-                  facecolor=(0.88,0.88,0.88,0.5),
-                  x_fraction=0.98,
-                  y_fraction=0.03,
-                  fontsize=6,
-                  horizontalalignment='right',
-                  verticalalignment='bottom')
+    #mg.utils.plot_label(ax_input,
+    #             '%5.2f %5.2f' % (numpy.mean(w_in.data),
+    #                                          numpy.std(w_in.data)),
+    #              facecolor=(0.88,0.88,0.88,0.5),
+    #              x_fraction=0.98,
+    #              y_fraction=0.03,
+    #              fontsize=6,
+    #              horizontalalignment='right',
+    #              verticalalignment='bottom')
 
+# Plot the hidden layer weights
+def plot_hidden(weights):
+     # Single axes - var v. time
+     ax=fig.add_axes([0.05,0.425,0.9,0.15])
+     # Axes ranges from data
+     ax.set_xlim(-0.6,len(weights)-0.4)
+     ax.set_ylim(0,numpy.max(numpy.abs(weights))*1.05)
+     ax.bar(x=range(len(weights)),
+            height=numpy.abs(weights[order]),
+            color='grey',
+            tick_label=order)
 
 # Get a 20CR data for the grid metadata
 ic=twcr.load('prmsl',datetime.datetime(1969,3,12,6),
                            version='2c')
 ic=ic.extract(iris.Constraint(member=1))
 
-# Get the 4 neuron autoencoder
+# Get the 9 neuron autoencoder
 model_save_file=(("%s/Machine-Learning-experiments/"+
                   "simple_autoencoder_introspection/9_neurons/"+
                  "saved_models/Epoch_%04d")) % (
                      os.getenv('SCRATCH'),100)
 autoencoder=tf.keras.models.load_model(model_save_file)
 
+# Get the order of the hidden weights - most to least important
+order=numpy.argsort(numpy.abs(autoencoder.get_weights()[1]))[::-1]
+
 # Make a comparison plot - Input, hidden, and output weights
-fig=Figure(figsize=(10,10),  # Width, Height (inches)
+fig=Figure(figsize=(10,12),  # Width, Height (inches)
            dpi=100,
            facecolor=(0.88,0.88,0.88,1),
            edgecolor=None,
@@ -93,18 +109,28 @@ fig=Figure(figsize=(10,10),  # Width, Height (inches)
            tight_layout=None)
 canvas=FigureCanvas(fig)
 
+# Hidden layer
+plot_hidden(autoencoder.get_weights()[1])
+
 # Global projection
 projection=ccrs.RotatedPole(pole_longitude=180.0, pole_latitude=90.0)
 extent=[-180,180,-90,90]
 
 for layer in [0,2]:
-    for channel in range(9):
+    w_l=autoencoder.get_weights()[layer]
+    vmin=numpy.mean(w_l)-numpy.std(w_l)*3
+    vmax=numpy.mean(w_l)+numpy.std(w_l)*3
+    count=0
+    for channel in order:
         w_in=ic.copy()
         if layer==0:
-            w_in.data=autoencoder.get_weights()[0][:,channel].reshape(ic.data.shape)
+            w_in.data=w_l[:,channel].reshape(ic.data.shape)
         else:
-            w_in.data=autoencoder.get_weights()[2][channel,:].reshape(ic.data.shape)
-        plot_weights(w_in,layer=layer,channel=channel,nchannels=9)
+            w_in.data=w_l[channel,:].reshape(ic.data.shape)
+        w_in.data *= numpy.sign(autoencoder.get_weights()[1][channel])
+        plot_weights(w_in,layer=layer,channel=count,nchannels=9,
+                     vmin=vmin,vmax=vmax)
+        count += 1
         
 # Render the figure as a png
 fig.savefig("weights.png")
