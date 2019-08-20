57faac022682107a47fdb4530da45df9329440ea
Philip Brohan <philip@brohan.org>
2019-08-20 16:05:43 +0100

    Added multivariate variational autoencoder.

---
+++ experiments/variational_autoencoder_perturbations/multivariate/autoencoder.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+# Variational autoencoder for 20CR prmsl fields.
+# This version does pressure, temperature, and precip.
+
+# This should be a generative model.
+
+import os
+import sys
+import tensorflow as tf
+#tf.enable_eager_execution()
+from tensorflow.data import Dataset
+import pickle
+import numpy
+from glob import glob
+
+# How many epochs to train for
+n_epochs=100
+
+# Dimensionality of the latent space
+latent_dim=100
+
+# Target data setup
+buffer_size=100
+batch_size=1
+
+# Training datasets with three variables
+input_file_dir=(("%s/Machine-Learning-experiments/datasets/rotated_pole/" +
+                "20CR2c/prmsl/training/") %
+                   os.getenv('SCRATCH'))
+prmsl_files=glob("%s/*.tfd" % input_file_dir)
+n_steps=len(prmsl_files)
+tr_tfd = tf.constant(prmsl_files)
+
+# Create TensorFlow Dataset object from the file names
+tr_data = Dataset.from_tensor_slices(tr_tfd).repeat(n_epochs)
+
+# From the prmsl file name, make a three-variable tensor
+def load_tensor(file_name):
+    sict  = tf.read_file(file_name)
+    prmsl = tf.parse_tensor(sict,numpy.float32)
+    prmsl = tf.reshape(prmsl,[79,159,1])
+    file_name = tf.strings.regex_replace(file_name,
+                                      'prmsl','air.2m')
+    sict  = tf.read_file(file_name)
+    t2m   = tf.parse_tensor(sict,numpy.float32)
+    t2m   = tf.reshape(t2m,[79,159,1])
+    file_name = tf.strings.regex_replace(file_name,
+                                      'air.2m','prate')
+    sict  = tf.read_file(file_name)
+    prate = tf.parse_tensor(sict,numpy.float32)
+    prate = tf.math.maximum(prate,0)
+    prate *= 1000
+    prate = tf.math.sqrt(prate)
+    prate = tf.reshape(prate,[79,159,1])
+    ict = tf.concat([prmsl,t2m,prate],2) # Now [79,159,3]
+    ict = tf.reshape(ict,[79,159,3])
+    return ict
+
+tr_data = tr_data.map(load_tensor)
+tr_data = tr_data.shuffle(buffer_size).batch(batch_size)
+tr_data = Dataset.zip((tr_data, tr_data))
+
+# Same for the test dataset
+input_file_dir=(("%s/Machine-Learning-experiments/datasets/rotated_pole/" +
+                "20CR2c/prmsl/test/") %
+                   os.getenv('SCRATCH'))
+prmsl_files=glob("%s/*.tfd" % input_file_dir)
+test_steps=len(prmsl_files)
+test_tfd = tf.constant(prmsl_files)
+test_data = Dataset.from_tensor_slices(test_tfd).repeat(n_epochs)
+test_data = test_data.map(load_tensor)
+test_data = test_data.batch(batch_size)
+test_data = Dataset.zip((test_data, test_data))
+
+# reparameterization trick
+# instead of sampling from Q(z|X), sample eps = N(0,I)
+# z = z_mean + sqrt(var)*eps
+def sampling(args):
+    z_mean, z_log_var = args
+    batch = tf.keras.backend.shape(z_mean)[0]
+    dim = tf.keras.backend.int_shape(z_mean)[1]
+    # by default, random_normal has mean=0 and std=1.0
+    epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+    return z_mean + tf.keras.backend.exp(0.5 * z_log_var) * epsilon
+
+# Input placeholder
+original = tf.keras.layers.Input(shape=(79,159,3,), name='encoder_input')
+# Encoding layers
+x = tf.keras.layers.Conv2D(27, (3, 3), padding='same')(original)
+x = tf.keras.layers.LeakyReLU()(x)
+x = tf.keras.layers.Conv2D(9, (3, 3), strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.LeakyReLU()(x)
+x = tf.keras.layers.Conv2D(3, (3, 3), strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.LeakyReLU()(x)
+x = tf.keras.layers.Conv2D(1, (3, 3), strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.LeakyReLU()(x)
+
+# N-dimensional latent space representation
+x = tf.keras.layers.Reshape(target_shape=(9*19*1,))(x)
+z_mean = tf.keras.layers.Dense(latent_dim, name='z_mean')(x)
+z_log_var = tf.keras.layers.Dense(latent_dim, name='z_log_var')(x)
+
+# Use reparameterization trick to push the sampling out as input
+z = tf.keras.layers.Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+
+# Define an encoder model
+encoder = tf.keras.models.Model(original, [z_mean, z_log_var, z], name='encoder')
+
+# Decoding layers
+encoded=tf.keras.layers.Input(shape=(latent_dim,), name='decoder_input') # Will be 'z' above
+x = tf.keras.layers.Dense(9*19*1)(encoded)
+x = tf.keras.layers.Reshape(target_shape=(9,19,1,))(x)
+x = tf.keras.layers.Conv2DTranspose(3, (3, 3),  strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.LeakyReLU()(x)
+x = tf.keras.layers.Conv2DTranspose(9, (3, 3),  strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.LeakyReLU()(x)
+x = tf.keras.layers.Conv2DTranspose(27, (3, 3),  strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.LeakyReLU()(x)
+decoded = tf.keras.layers.Conv2D(3, (3, 3), padding='same')(x) # Will be 75x159x3 - same as input
+
+# Define a generator (decoder) model
+generator = tf.keras.models.Model(encoded, decoded, name='generator')
+
+# Combine the encoder and the generator into an autoencoder
+output = generator(encoder(original)[2])
+autoencoder = tf.keras.models.Model(inputs=original, outputs=output, name='autoencoder')
+
+# Specify a loss function - combination of the reconstruction loss
+#  and the KL divergence of the latent space (from a multivariate gaussian).
+
+reconstruction_loss = tf.keras.losses.mse(original, output)
+reconstruction_loss *= latent_dim * 100
+
+kl_loss = 1 + z_log_var - tf.keras.backend.square(z_mean) - tf.keras.backend.exp(z_log_var)
+kl_loss = tf.keras.backend.sum(kl_loss, axis=-1)
+kl_loss *= -0.5
+
+vae_loss = tf.keras.backend.mean(reconstruction_loss + kl_loss)
+    
+autoencoder.add_loss(vae_loss)
+
+autoencoder.compile(optimizer='adadelta')
+
+# Save model and history state after every epoch
+history={}
+history['loss']=[]
+history['val_loss']=[]
+class CustomSaver(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        save_dir=("%s/Machine-Learning-experiments/"+
+                   "variational_autoencoder_perturbations/"+
+                   "multivariate/saved_models/Epoch_%04d") % (
+                         os.getenv('SCRATCH'),epoch)
+        if not os.path.isdir(os.path.dirname(save_dir)):
+            os.makedirs(os.path.dirname(save_dir))
+        for model in ['autoencoder','encoder','gererator']:
+            if not os.path.isdir(os.path.dirname("%s/%s" % (save_dir,model))):
+                os.makedirs(os.path.dirname("%s/%s" % (save_dir,model)))
+        tf.keras.models.save_model(autoencoder,"%s/autoencoder" % save_dir)
+        tf.keras.models.save_model(encoder,"%s/encoder" % save_dir)
+        tf.keras.models.save_model(generator,"%s/generator" % save_dir)
+        history['loss'].append(logs['loss'])
+        history['val_loss'].append(logs['val_loss'])
+        history_file=("%s/Machine-Learning-experiments/"+
+                      "variational_autoencoder_perturbations/"+
+                      "multivariate/saved_models/history_to_%04d.pkl") % (
+                         os.getenv('SCRATCH'),epoch)
+        pickle.dump(history, open(history_file, "wb"))
+        
+saver = CustomSaver()
+
+# Train the autoencoder
+history=autoencoder.fit(x=tr_data,
+                epochs=n_epochs,
+                steps_per_epoch=n_steps,
+                validation_data=test_data,
+                validation_steps=test_steps,
+                verbose=2,
+                callbacks=[saver])
+
---
+++ experiments/variational_autoencoder_perturbations/multivariate/validation/compare_results.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+# Model training results plot
+
+import tensorflow as tf
+tf.enable_eager_execution()
+import numpy
+
+import IRData.twcr as twcr
+import iris
+import datetime
+import argparse
+import os
+import math
+import pickle
+
+import Meteorographica as mg
+from pandas import qcut
+
+import matplotlib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import cartopy
+import cartopy.crs as ccrs
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", help="Epoch",
+                    type=int,required=True)
+args = parser.parse_args()
+
+# Function to resize and rotate pole
+def rr_cube(cbe):
+    # Use the Cassini projection (boundary is the equator)
+    cs=iris.coord_systems.RotatedGeogCS(0.0,60.0,270.0)
+    # Latitudes cover -90 to 90 with 79 values
+    lat_values=numpy.arange(-90,91,180/78)
+    latitude = iris.coords.DimCoord(lat_values,
+                                    standard_name='latitude',
+                                    units='degrees_north',
+                                    coord_system=cs)
+    # Longitudes cover -180 to 180 with 159 values
+    lon_values=numpy.arange(-180,181,360/158)
+    longitude = iris.coords.DimCoord(lon_values,
+                                     standard_name='longitude',
+                                     units='degrees_east',
+                                     coord_system=cs)
+    dummy_data = numpy.zeros((len(lat_values), len(lon_values)))
+    dummy_cube = iris.cube.Cube(dummy_data,
+                               dim_coords_and_dims=[(latitude, 0),
+                                                    (longitude, 1)])
+    n_cube=cbe.regrid(dummy_cube,iris.analysis.Linear())
+    return(n_cube)
+
+# Function to do the multivariate plot
+def three_plot(ax,prmsl,prate,t2m):
+    ax.set_axis_off() # Don't want surrounding x and y axis
+    ax.background_patch.set_facecolor((0.88,0.88,0.88,1))
+    land_img=ax.background_img(name='GreyT', resolution='low')
+    # Plot the temperature
+    plot_cube=mg.utils.dummy_cube(ax,0.25)
+    t2m = t2m.regrid(plot_cube,iris.analysis.Linear())
+    # Re-map to highlight small differences
+    s=t2m.data.shape
+    t2m.data=qcut(t2m.data.flatten(),20,labels=False).reshape(s)
+    # Plot as a colour map
+    lats = t2m.coord('latitude').points
+    lons = t2m.coord('longitude').points
+    t2m_img=ax.pcolorfast(lons, lats, t2m.data,
+                          cmap='coolwarm',
+                          vmin=0,
+                          vmax=20,
+                          alpha=0.5)
+    # Also pressure
+    mg.pressure.plot(ax,prmsl,scale=0.01,resolution=0.25,
+                     linewidths=1)
+    # Also precip
+    mg.precipitation.plot(ax,prate,resolution=0.25,vmin=-0.01,vmax=0.04)
+
+ 
+# Get the 20CR data
+prmsl=twcr.load('prmsl',datetime.datetime(2009,3,12,18),
+                           version='2c')
+prmsl=rr_cube(prmsl.extract(iris.Constraint(member=1)))
+prate=twcr.load('prate',datetime.datetime(2009,3,12,18),
+                           version='2c')
+prate=rr_cube(prate.extract(iris.Constraint(member=1)))
+t2m=twcr.load('air.2m',datetime.datetime(2009,3,12,18),
+                           version='2c')
+t2m=rr_cube(t2m.extract(iris.Constraint(member=1)))
+
+# Get the autoencoder
+model_save_file=("%s/Machine-Learning-experiments/"+
+                  "variational_autoencoder_perturbations/"+
+                  "multivariate/saved_models/Epoch_%04d/autoencoder") % (
+                      os.getenv('SCRATCH'),args.epoch)
+autoencoder=tf.keras.models.load_model(model_save_file,compile=False)
+
+# Get the encoded fields 
+prmsl_t = prmsl.copy()
+prmsl_t.data -= 101325
+prmsl_t.data /= 3000
+prmsl_t = tf.convert_to_tensor(prmsl_t.data, numpy.float32)
+prmsl_t = tf.reshape(prmsl_t,[79,159,1])
+t2m_t = t2m.copy()
+t2m_t.data -= 280
+t2m_t.data /=  50
+t2m_t = tf.convert_to_tensor(t2m_t.data, numpy.float32)
+t2m_t = tf.reshape(t2m_t,[79,159,1])
+prate_t = prate.copy()
+prate_t.data = tf.math.maximum(prate_t.data,0)
+prate_t.data *= 1000
+prate_t.data = tf.math.sqrt(prate_t.data)
+prate_t = tf.convert_to_tensor(prate_t.data, numpy.float32)
+prate_t = tf.reshape(prate_t,[79,159,1])
+ict = tf.concat([prmsl_t,t2m_t,prate_t],2) # Now [79,159,3]
+ict = tf.reshape(ict,[1,79,159,3])
+result = autoencoder.predict_on_batch(ict)
+result = tf.reshape(result,[79,159,3])
+prmsl_r = prmsl.copy()
+prmsl_r.data = tf.reshape(result.numpy()[:,:,0],[79,159]).numpy()
+prmsl_r.data *= 3000
+prmsl_r.data += 101325
+t2m_r = t2m.copy()
+t2m_r.data = tf.reshape(result.numpy()[:,:,1],[79,159]).numpy()
+t2m_r.data *= 50
+t2m_r.data += 280
+prate_r = prate.copy()
+prate_r.data = tf.reshape(result.numpy()[:,:,2],[79,159]).numpy()
+prate_r.data = prate_r.data**2
+prate_r.data /= 1000
+
+fig=Figure(figsize=(9.6*1.2,10.8),
+           dpi=100,
+           facecolor=(0.88,0.88,0.88,1),
+           edgecolor=None,
+           linewidth=0.0,
+           frameon=False,
+           subplotpars=None,
+           tight_layout=None)
+canvas=FigureCanvas(fig)
+
+# Two maps, original and reconstructed
+matplotlib.rc('image',aspect='auto')
+projection=ccrs.RotatedPole(pole_longitude=60.0,
+                            pole_latitude=0.0,
+                            central_rotated_longitude=270.0)
+extent=[-180,180,-90,90]
+ax_original=fig.add_axes([0.005,0.525,0.75,0.45],projection=projection)
+ax_original.set_extent(extent, crs=projection)
+three_plot(ax_original,prmsl,prate,t2m)
+ax_reconstructed=fig.add_axes([0.005,0.025,0.75,0.45],projection=projection)
+ax_reconstructed.set_extent(extent, crs=projection)
+three_plot(ax_reconstructed,prmsl_r,prate_r,t2m_r)
+
+# Scatterplot of encoded v original
+def plot_scatter(ax,ic,pm):
+    dmin=min(ic.data.min(),pm.data.min())
+    dmax=max(ic.data.max(),pm.data.max())
+    dmean=(dmin+dmax)/2
+    dmax=dmean+(dmax-dmean)*1.05
+    dmin=dmean-(dmean-dmin)*1.05
+    ax.set_xlim(dmin,dmax)
+    ax.set_ylim(dmin,dmax)
+    ax.scatter(x=pm.data.flatten(),
+               y=ic.data.flatten(),
+               c='black',
+               alpha=0.25,
+               marker='.',
+               s=2)
+    ax.set(ylabel='Original', 
+           xlabel='Encoded')
+    ax.grid(color='black',
+            alpha=0.2,
+            linestyle='-', 
+            linewidth=0.5)
+    
+ax_prmsl=fig.add_axes([0.82,0.82,0.16,0.17])
+plot_scatter(ax_prmsl,prmsl/100,prmsl_r/100) # hPa
+ax_t2m=fig.add_axes([0.82,0.575,0.16,0.17])
+plot_scatter(ax_t2m,t2m-273.18,t2m_r-273.18) # degC
+ax_prate=fig.add_axes([0.82,0.33,0.16,0.17])
+plot_scatter(ax_prate,prate*1000,prate_r*1000) # arbitrary
+
+# Plot the training history
+history_save_file=("%s/Machine-Learning-experiments/"+
+                   "variational_autoencoder_perturbations/"+
+                   "multivariate/saved_models/history_to_%04d.pkl") % (
+                       os.getenv('SCRATCH'),args.epoch)
+history=pickle.load( open( history_save_file, "rb" ) )
+ax=fig.add_axes([0.82,0.05,0.16,0.21])
+# Axes ranges from data
+ax.set_xlim(0,len(history['loss']))
+ax.set_ylim(0,numpy.max(numpy.concatenate((history['loss'],
+                                           history['val_loss']))))
+ax.set(xlabel='Epoch', 
+       ylabel='Loss (grey) and validation loss (black)')
+ax.grid(color='black',
+        alpha=0.2,
+        linestyle='-', 
+        linewidth=0.5)
+ax.plot(range(len(history['loss'])),
+        history['loss'],
+        color='grey',
+        linestyle='-',
+        linewidth=2)
+ax.plot(range(len(history['val_loss'])),
+        history['val_loss'],
+        color='black',
+        linestyle='-',
+        linewidth=2)
+
+
+# Render the figure as a png
+fig.savefig("comparison_results.png")
+
