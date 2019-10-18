cd6e7404dd4aac72da2ad6491832a692109f952a
Philip Brohan <philip@brohan.org>
2019-10-18 15:42:12 +0100

    Added LSTM predictor incoporating time as a predictor.

---
+++ experiments/DWR_LSTM/1_ahead_56_behind+date/validation/plot_series_t2m.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+
+# Plot the pseudo-DWR obs for one station for a selected period
+
+import os
+import pickle
+import datetime
+import pandas
+import numpy
+import tensorflow as tf
+
+import matplotlib
+from matplotlib.backends.backend_agg import \
+                 FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.patches import Polygon
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--start", help="Start time: YYYY-MM-DD:HH",
+                    type=str,required=True)
+parser.add_argument("--end", help="End time: YYYY-MM-DD:HH",
+                    type=str,required=True)
+parser.add_argument("--station", help="Station series to show",
+                    type=str,required=False,default='LONDON')
+parser.add_argument("--epoch", help="Use saved model at which epoch",
+                    type=int,required=True)
+args = parser.parse_args()
+args.start=datetime.datetime.strptime(args.start,"%Y-%m-%d:%H")
+args.end  =datetime.datetime.strptime(args.end,  "%Y-%m-%d:%H")
+
+# Load the data
+# Get the position in the anual cycle
+def get_annual(dates,year):
+    dt=dates-numpy.datetime64('%04d-01-01' % year)
+    days=dt.values/numpy.timedelta64(1,'D')
+    annual=days/365
+    return annual
+
+obs=None
+year=args.start.year
+while year<=args.end.year:
+    of_name=(("%s/Machine-Learning-experiments/datasets/"+
+             "DWR/20CRv2c/air.2m/%04d.pkl") %
+                       (os.getenv('SCRATCH'),year))
+    if not os.path.isfile(of_name):
+        raise IOError("No obs file for given version and date")
+    obs_f = pickle.load( open( of_name, "rb" ) )
+    obs_f = obs_f[(obs_f['Date']>=args.start) & (obs_f['Date']<args.end)]
+    obs_f['annual']=get_annual(obs_f['Date'],year)
+    obs_f['diurnal']=obs_f['Date'].apply(lambda x: x.hour/24)
+    obs_f['random']=numpy.random.rand(len(obs_f['Date']))
+    if obs is None:
+        obs=obs_f
+    else:
+        obs=pandas.concat([obs,obs_f])
+    year += 1
+
+# Load the saved model
+save_file=("%s/Machine-Learning-experiments/"+
+           "DWR_LSTM/1_ahead_56_behind+date/air.2m/"+
+           "saved_models/Epoch_%04d") % (
+                 os.getenv('SCRATCH'),args.epoch)
+predictor=tf.keras.models.load_model(save_file)
+# Retrieve the model metadata
+meta_file="%s/meta_%04d" % (os.path.dirname(save_file),args.epoch)
+model_meta=pickle.load( open( meta_file, "rb" ) )
+
+# For each data point in the selected period, calculate a prediction
+obs[args.station] -= model_meta['s_mean']
+obs[args.station] /= model_meta['s_std']
+prediction=[]
+indices = ['random','annual','diurnal',args.station]
+#indices = ['random','annual','diurnal']
+for i in range(model_meta['source_len'], 
+               len(obs[args.station])-model_meta['target_len']):
+    source=numpy.reshape(obs[indices].values[i-model_meta['source_len']:i], 
+                         (1,len(indices), model_meta['source_len']))
+    prediction.append(predictor.predict(source)[0])
+prediction=numpy.array(prediction).flatten()
+
+# Unnormalise the data
+target=obs[args.station].values*model_meta['s_std']+model_meta['s_mean']
+prediction = prediction*model_meta['s_std']+model_meta['s_mean']
+
+# Make the plot
+aspect=16.0/9.0
+fig=Figure(figsize=(10.8*aspect,10.8),  # Width, Height (inches)
+           dpi=100,
+           facecolor=(0.88,0.88,0.88,1),
+           edgecolor=None,
+           linewidth=0.0,
+           frameon=False,
+           subplotpars=None,
+           tight_layout=None)
+canvas=FigureCanvas(fig)
+font = {'family' : 'sans-serif',
+        'sans-serif' : 'Arial',
+        'weight' : 'normal',
+        'size'   : 14}
+matplotlib.rc('font', **font)
+
+# Single axes - var v. time
+ax=fig.add_axes([0.05,0.05,0.945,0.74])
+ax.set_xlim(min(obs['Date'].values),
+            max(obs['Date'].values))
+ax.set_ylim(min(min(prediction),min(target)-model_meta['persistence_error']*2)-273.15-1,
+            max(max(prediction),max(target)+model_meta['persistence_error']*2)-273.15+1)
+ax.set_ylabel('2m Temperature (C)')
+ax.plot(obs['Date'].values,
+                (target+model_meta['persistence_error']*2)-273.15,
+                markersize=0,
+                color='grey',
+                marker='.',
+                linewidth=0.5,
+                alpha=1.0,
+                zorder=30)
+ax.plot(obs['Date'].values,
+                (target-model_meta['persistence_error']*2)-273.15,
+                markersize=0,
+                color='grey',
+                marker='.',
+                linewidth=0.5,
+                alpha=1.0,
+                zorder=30)
+
+ax.plot(obs['Date'].values,
+                target-273.15,
+                markersize=10,
+                color='black',
+                marker='.',
+                linewidth=1.0,
+                alpha=1.0,
+                zorder=50)
+ax.plot(obs['Date'].values[model_meta['source_len']:(len(obs[args.station])-model_meta['target_len'])],
+                prediction-273.15,
+                markersize=10,
+                color='blue',
+                marker='.',
+                linewidth=1.0,
+                alpha=1.0,
+                zorder=60)
+# Also plot the error - compared with persistence
+persistence = target[model_meta['target_len']:]-target[:(model_meta['target_len']*-1)]
+delta_p=persistence
+delta_m=target[model_meta['source_len']:(len(obs[args.station])-model_meta['target_len'])]-prediction
+ax2=fig.add_axes([0.05,0.80,0.945,0.195])
+ax2.set_xlim(min(obs['Date'].values),
+             max(obs['Date'].values))
+ax2.get_xaxis().set_visible(False)
+ax2.set_ylim(min(min(delta_p),min(delta_m))-2,
+             max(max(delta_p),max(delta_m))+2)
+ax2.set_ylabel('2m Temperature (C)')
+ax2.plot(numpy.array([min(obs['Date'].values),max(obs['Date'].values)]),
+         numpy.array([0,0]),
+                markersize=0,
+                color='grey',
+                marker='.',
+                linewidth=0.5,
+                alpha=1.0,
+                zorder=30)
+ax2.plot(obs['Date'].values[model_meta['target_len']:],
+                delta_p,
+                markersize=6,
+                color='black',
+                marker='.',
+                linewidth=0.5,
+                alpha=1.0,
+                zorder=50)
+ax2.plot(obs['Date'].values[model_meta['source_len']:(len(obs[args.station])-model_meta['target_len'])],
+                delta_m,
+                markersize=5,
+                color='blue',
+                marker='.',
+                linewidth=0.5,
+                alpha=1.0,
+                zorder=60)
+
+
+fig.savefig('Predictions_air.2m.png')
